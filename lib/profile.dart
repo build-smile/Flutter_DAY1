e7e9a3dedc9e0645721512703e7b11c6c7c95a9d
Laotrakul <natchapat.laotrakul@accenture.com>
2022-05-07 10:19:05 +0700

    counterScreen

--- lib/profile.dart
+++ lib/profile.dart
@@ -10,6 +10,36 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.blue,
+                height: 100,
+                child: Text('Hello'),
+              ),
+            ),
+            Expanded(
+                flex: 3,
+                child: Container(
+                  color: Colors.red,
+                  height: 100,
+                  child: Text('Hello'),
+                )),
+            Expanded(
+              child: Container(
+                color: Colors.green,
+                height: 100,
+                child: Text('Hello'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
