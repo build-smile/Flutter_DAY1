be7f2440a5ac8e57d5dd0d7f7332ab94619be6ac
Laotrakul <natchapat.laotrakul@accenture.com>
2022-05-07 11:37:16 +0700

    complete

--- lib/contactus.dart
+++ lib/contactus.dart
@@ -8,13 +8,62 @@ class ContactUsScreen extends StatefulWidget {
 }
 
 class _ContactUsScreenState extends State<ContactUsScreen> {
+  var _key = GlobalKey<FormState>();
+  String name = '';
+  String email = '';
   @override
   Widget build(BuildContext context) {
     String text = ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(text),
       ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [
+              TextFormField(
+                onSaved: (String? value) {
+                  name = value!;
+                },
+                validator: validateString,
+                decoration:
+                    InputDecoration(hintText: 'Name', labelText: 'Name'),
+              ),
+              TextFormField(
+                onSaved: (String? value) {
+                  email = value!;
+                },
+                validator: validateString,
+                decoration:
+                    InputDecoration(hintText: 'Email', labelText: 'Email'),
+              ),
+              ElevatedButton(onPressed: submit, child: Text('Submit'))
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  submit() {
+    if (_key.currentState!.validate()) {
+      _key.currentState!.save();
+      print('username: $name email:$email');
+    }
+  }
+
+  String? validateString(String? value) {
+    if (value == null || value == '') {
+      return 'Please fill data';
+    }
+    if (value.length > 10) {
+      return 'length limit 10';
+    }
+
+    return null;
   }
 }
