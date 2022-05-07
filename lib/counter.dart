eaa7a1446832a063588d5deb2aaad5351afa8fc6
Laotrakul <natchapat.laotrakul@accenture.com>
2022-05-07 10:01:02 +0700

    counterScreen

---
+++ lib/counter.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          'Presses the button $count time',
+          style: TextStyle(fontSize: 20, color: Colors.blue),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          count++;
+          print(count);
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
